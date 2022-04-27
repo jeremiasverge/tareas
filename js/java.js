0f3f54a122a0053610ea02ad898ff2ff8c545640
unknown <Jeremias Verge@JEREPC.JEREMIAS_VERGE>
2022-04-26 23:37:28 -0300

    first commit

---
+++ js/java.js
@@ -0,0 +1,10 @@
+var div = document.createElement('div');
+var p = document.createElement('p');
+var p1 = document.createElement('p');
+body.appendChild(div);
+div.appendChild(p);
+div.appendChild(p1);
+p.textContent = 'tarea realizada';
+p1.innerHTML = ' <h1> no lo veras jamas </h1>';
+
+
